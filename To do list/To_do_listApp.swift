580e09e0e26f6b71272be5cb9837003cf1e0cfcb
MattBartie <mattbartie3@gmail.com>
2023-12-18 13:33:52 -0500

    Initial Commit

---
+++ To do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To do list
+//
+//  Created by Matt Bartie on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
